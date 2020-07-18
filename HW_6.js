051e212c38842e26641c0886377722c032ac91f4
romantrynko <trynkoroman25@gmail.com>
2020-07-18 16:00:05 +0300

    class_part_done

--- HW_6.js
+++ HW_6.js
@@ -82,7 +82,7 @@ let wordsArray = ['Hello', 'my', 'name', 'is', 'Roman', 'i', 'live', 'and', 'wor
 
 // 13 -- отфильтровать слова длиной менее 4х символов
 
-// let filterLessFour = wordsArray.filter((value) => value.length < 3 ? value : '')
+// let filterLessFour = wordsArray.filter((value) => value.length < 4 ? value : '')
 
 // console.log(filterLessFour);
 
@@ -101,19 +101,19 @@ let wordsArray = ['Hello', 'my', 'name', 'is', 'Roman', 'i', 'live', 'and', 'wor
 
 // Дан масив :
 
-let users = [
-    { name: 'vasya', age: 31, status: false },
-    { name: 'petya', age: 30, status: true },
-    { name: 'kolya', age: 29, status: true },
-    { name: 'olya', age: 28, status: false },
-    { name: 'max', age: 30, status: true },
-    { name: 'anya', age: 31, status: false },
-    { name: 'oleg', age: 28, status: false },
-    { name: 'andrey', age: 29, status: true },
-    { name: 'masha', age: 30, status: true },
-    { name: 'olya', age: 31, status: false },
-    { name: 'max', age: 31, status: true }
-];
+// let users = [
+//     { name: 'vasya', age: 31, status: false },
+//     { name: 'petya', age: 30, status: true },
+//     { name: 'kolya', age: 29, status: true },
+//     { name: 'olya', age: 28, status: false },
+//     { name: 'max', age: 30, status: true },
+//     { name: 'anya', age: 31, status: false },
+//     { name: 'oleg', age: 28, status: false },
+//     { name: 'andrey', age: 29, status: true },
+//     { name: 'masha', age: 30, status: true },
+//     { name: 'olya', age: 31, status: false },
+//     { name: 'max', age: 31, status: true }
+// ];
 
 // 19 - відсортувати його за  віком (зростання, а потім окремо спадання)
 
@@ -177,6 +177,8 @@ let users = [
 // console.log(calcThree(5, 5, 8, (num1, num2, num3) => (num1 + num2 + num3) * num3));
 
 
+
+// 37
 
 let cars = [
     {
@@ -308,3 +310,141 @@ let cars = [
         power: 230
     }
 ];
+
+// Відфільтрувати масив за наступними критеріями :
+// - двигун більше 3х літрів
+
+let engineLessThreeLit = cars.filter(car => car.volume > 3);
+console.log(engineLessThreeLit);
+
+// - двигун = 2л
+
+let engineTwoLit = cars.filter(car => car.volume === 2);
+console.log(engineTwoLit);
+
+// - виробник мерс
+
+let producerMerc = cars.filter(car => car.producer === 'mercedes');
+console.log(producerMerc);
+
+// - двигун більше 3х літрів + виробник мерседес
+
+let engMoreThreeProdMerc = cars.filter(car => car.volume > 3 && car.producer === 'mercedes')
+console.log(engMoreThreeProdMerc);
+
+// - двигун більше 3х літрів + виробник субару
+
+let engMoreThreeProdSub = cars.filter(car => car.volume > 3 && car.producer === 'subaru')
+console.log(engMoreThreeProdSub);
+
+// - сили більше ніж 300
+
+let threeHundredHP = cars.filter(car => car.power > 300);
+console.log(threeHundredHP);
+
+// - сили більше ніж 300 + виробник субару
+
+let threeHPProdSub = cars.filter(car => car.power > 300 && car.producer === 'subaru');
+console.log(threeHPProdSub);
+
+// - мотор серіі ej
+
+let engEj = cars.filter(car => car.engine.toLowerCase().startsWith('ej'));
+console.log(engEj);
+
+// - сили більше ніж 300 + виробник субару + мотор серіі ej
+
+let powerProdEng = cars.filter(car => car.power > 300 && car.producer === 'subaru' && car.engine.startsWith('ej'));
+console.log(powerProdEng);
+
+// - двигун меньше 3х літрів + виробник мерседес
+
+let engLessTwoMerc = cars.filter(car => car.volume < 3 && car.producer ==='mercedes');
+console.log(engLessTwoMerc);
+
+// - двигун більше 2л + сили більше 250
+
+let engTwoPower = cars.filter(car => car.volume > 2 && car.power > 250);
+console.log(engTwoPower); 
+
+// - сили більше 250  + виробник бмв
+
+let powerBmw = cars.filter(car => car.power > 250 && car.producer === 'bmw');
+console.log(powerBmw);
+
+
+
+// - взять слдующий массив
+let usersWithAddress = [
+    {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}}, 
+    {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Lviv', street: 'Naukova', number: 1}}, 
+    {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Lubinska', number: 121}}, 
+    {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Lviv', street: 'Kulisha', number: 90}}, 
+    {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Bandery', number: 115}}, 
+    {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Lviv', street: 'Levandivska', number: 2}}, 
+    {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Lviv', street: 'Gorodotska', number: 22}}, 
+    {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Ruska', number: 43}}, 
+    {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Lviv', street: 'Sakharova', number: 12}}, 
+    {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Zelena', number: 16}}, 
+    {id: 11, name: 'max', age: 31, status: true, address: {city: 'Lviv', street: 'Antonovycha', number: 121}}
+];
+
+// -- отсортировать его по id пользователей
+
+// let sortedUWA = usersWithAddress.sort((user1, user2) => user1.id - user2.id);
+// console.log(sortedUWA);
+
+// -- отсортировать его по id пользователей в обратном опрядке
+
+// let sortedRevUWA = usersWithAddress.sort((user1, user2) => user2.id - user1.id);
+// console.log(sortedRevUWA);
+
+// -- отсортировать его по возрасту пользователей
+
+// let ageSorted = usersWithAddress.sort((user1, user2) => user1.age - user2.age);
+// console.log(ageSorted);
+
+// -- отсортировать его по возрасту пользователей в обратном порядке
+
+// let ageRevSorted = usersWithAddress.sort((user1, user2) => user2.age - user1.age);
+// console.log(ageRevSorted);
+
+// -- отсортировать его по имени пользователей
+
+// let nameSorted = usersWithAddress.sort((user1, user2) => user1.name < user2.name ? -1 : 1);
+// console.log(nameSorted);
+
+// -- отсортировать его по имени пользователей в обратном порядке
+
+// let nameRevSorted = usersWithAddress.sort((user1, user2) => user2.name < user1.name ? -1 : 1);
+// console.log(nameRevSorted);
+
+// -- отсортировать его по названию улицы  в алфавитном порядке
+
+// let streetSorted = usersWithAddress.sort((user1, user2) => user1.address.street < user2.address.street ? -1 : 1);
+// console.log(streetSorted);
+
+// -- отсортировать его по номеру дома по возрастанию
+
+// let buildingSorted = usersWithAddress.sort((user1, user2) => user1.address.number - user2.address.number);
+// console.log(buildingSorted);
+
+// -- отфильтровать (оставить) тех кто младше 30
+
+// let ageFiltered = usersWithAddress.filter(user => user.age < 30);
+// console.log(ageFiltered);
+
+// -- отфильтровать (оставить) тех у кого отрицательный статус
+
+// let statusFiltered = usersWithAddress.filter(user => user.status === false);
+// console.log(statusFiltered);
+
+// -- отфильтровать (оставить) тех у кого отрицательный статус и младше 30 лет
+
+// let statusAgeFiltered = usersWithAddress.filter(user => user.status === false && user.age < 30);
+// console.log(statusAgeFiltered);
+
+// -- отфильтровать (оставить) тех у кого номер дома четный
+
+// let buildFiltered = usersWithAddress.filter(user => !(user.address.number % 2));
+// console.log(buildFiltered);
